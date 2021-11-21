7a00a199cda2e215dd581c9df732bf5ca02b5567
Suvorov S.K <s.k.suvorov.email@gmail.com>
2021-11-21 19:44:06 +0300

    Добавлена активация выделения предмета при выборе и диактивация при выборе другого предмета

---
+++ Assets/Scripts/UserControlSystem/UI/Model/SelectableValue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Abstractions;
+using UnityEngine;
+
+
+namespace UserControlSystem
+{
+    [CreateAssetMenu(fileName = nameof(SelectableValue), menuName = "Strategy Game/" + nameof(SelectableValue), order = 0)]
+    public class SelectableValue : ScriptableObject
+    {
+        public ISelectable CurrentValue { get; private set; }
+        public event Action<ISelectable> OnSelected;
+
+        public void SetValue(ISelectable value)
+        {
+            if (CurrentValue != null)
+                CurrentValue.OnDeselected();
+
+            CurrentValue = value;
+            OnSelected?.Invoke(value);
+
+            if(CurrentValue!=null)
+                CurrentValue.OnSelected();
+        }
+    }
+}
